e4bd1dc4992f1be4cd10c0696444dd117d5c1a62
Kemal Çelikel <kemalcelikel@outlook.com>
2020-12-22 14:36:28 +0300

    update parameter types

--- adam_io/requestor.py
+++ adam_io/requestor.py
@@ -2,7 +2,7 @@ import base64
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
-from .digital_io import DigitalOutput
+from typing import Dict, Optional
 from .utils import URI
 
 
@@ -21,13 +21,13 @@ class Requestor:
                         "Authorization": "Basic " + encoded_auth_str}
         self.base_url = f"http://{ip}"
 
-    def input(self, input_channel_id: int = None):
+    def input(self, input_channel_id: Optional[int] = None):
         """
         if the requests digital output index is out of bounds,
         request returns status code 501.
 
         :param input_channel_id: single input is requested, none returns all digital inputs
-        :return: ADAM response
+        :return: ADAM response, xml response with status code/message
         """
         if input_channel_id:
             input_channel_id = "/" + str(input_channel_id)
@@ -38,13 +38,13 @@ class Requestor:
         response = urlopen(request)
         return response.read().decode('utf8')
 
-    def output(self, data: DigitalOutput = None):
+    def output(self, data: Optional[Dict[str, int]] = None):
         """
         if the requests digital output index is out of bounds,
         request returns status code 501.
 
-        :param data: DigitalOutput object
-        :return: ADAM response
+        :param data: DigitalOutput object converted to dictionary as {"DO1":1,...}
+        :return: ADAM response, xml response with status code/message
         """
         url = self.base_url + URI.DIGITAL_OUTPUT + URI.ALL + URI.VALUE
 
